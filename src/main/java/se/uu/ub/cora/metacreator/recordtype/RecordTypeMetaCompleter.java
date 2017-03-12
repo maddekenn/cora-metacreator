a3b3fe00177bc6281da9455662288e9eba6d839a
madde <madde@4b7649b932d4>
2017-03-12 20:06:03 +0000

    Started working on changes in recordtype

--- src/main/java/se/uu/ub/cora/metacreator/recordtype/RecordTypeMetaCompleter.java
+++ src/main/java/se/uu/ub/cora/metacreator/recordtype/RecordTypeMetaCompleter.java
@@ -6,53 +6,63 @@ import se.uu.ub.cora.spider.extended.ExtendedFunctionality;
 
 public class RecordTypeMetaCompleter implements ExtendedFunctionality {
 
-    private String userId;
-    private SpiderDataGroup spiderDataGroup;
-    private String id;
+	private String userId;
+	private SpiderDataGroup spiderDataGroup;
+	private String id;
 
-    @Override
-    public void useExtendedFunctionality(String userId, SpiderDataGroup spiderDataGroup) {
-        this.userId = userId;
-        this.spiderDataGroup = spiderDataGroup;
-        
-        addValuesToDataGroup();
+	@Override
+	public void useExtendedFunctionality(String userId, SpiderDataGroup spiderDataGroup) {
+		this.userId = userId;
+		this.spiderDataGroup = spiderDataGroup;
+
+		addValuesToDataGroup();
 	}
-    
-    private void addValuesToDataGroup(){
-    	SpiderDataGroup recordInfoGroup = spiderDataGroup.extractGroup("recordInfo");
-    	id = recordInfoGroup.extractAtomicValue("id");
+
+	private void addValuesToDataGroup() {
+		SpiderDataGroup recordInfoGroup = spiderDataGroup.extractGroup("recordInfo");
+		id = recordInfoGroup.extractAtomicValue("id");
 		addMetadataIds();
 		addPresentationIds();
 		addTexts();
-    }
-
-	private void addMetadataIds() {
-		addAtomicValueWithNameInDataAndValueIfNotExisting("metadataId", id+"Group");
-		addAtomicValueWithNameInDataAndValueIfNotExisting("newMetadataId", id+"NewGroup");
-		addAtomicValueWithNameInDataAndValueIfNotExisting("searchMetadataId", id+"SearchGroup");
+		SpiderDataGroup search = SpiderDataGroup.withNameInData("search");
+		search.addChild(SpiderDataAtomic.withNameInDataAndValue("linkedRecordType", "search"));
+		search.addChild(SpiderDataAtomic.withNameInDataAndValue("linkedRecordId", id + "Search"));
+		spiderDataGroup.addChild(search);
 	}
 
-	private void addAtomicValueWithNameInDataAndValueIfNotExisting(String nameInData, String value) {
-		if(!spiderDataGroup.containsChildWithNameInData(nameInData)) {
+	private void addMetadataIds() {
+		addAtomicValueWithNameInDataAndValueIfNotExisting("metadataId", id + "Group");
+		addAtomicValueWithNameInDataAndValueIfNotExisting("newMetadataId", id + "NewGroup");
+		// addAtomicValueWithNameInDataAndValueIfNotExisting("searchMetadataId",
+		// id + "SearchGroup");
+	}
+
+	private void addAtomicValueWithNameInDataAndValueIfNotExisting(String nameInData,
+			String value) {
+		if (!spiderDataGroup.containsChildWithNameInData(nameInData)) {
 			spiderDataGroup.addChild(SpiderDataAtomic.withNameInDataAndValue(nameInData, value));
 		}
 	}
 
 	private void addPresentationIds() {
-		addAtomicValueWithNameInDataAndValueIfNotExisting("presentationViewId", id+"ViewPGroup");
+		addAtomicValueWithNameInDataAndValueIfNotExisting("presentationViewId", id + "ViewPGroup");
 
-		addAtomicValueWithNameInDataAndValueIfNotExisting("presentationFormId", id+"FormPGroup");
-		addAtomicValueWithNameInDataAndValueIfNotExisting("newPresentationFormId", id+"FormNewPGroup");
-		addAtomicValueWithNameInDataAndValueIfNotExisting("menuPresentationViewId", id+"MenuPGroup");
-		addAtomicValueWithNameInDataAndValueIfNotExisting("listPresentationViewId", id+"ListPGroup");
-		addAtomicValueWithNameInDataAndValueIfNotExisting("selfPresentationViewId", id+"ViewSelfPGroup");
-		addAtomicValueWithNameInDataAndValueIfNotExisting("searchPresentationFormId", id+"FormSearchPGroup");
+		addAtomicValueWithNameInDataAndValueIfNotExisting("presentationFormId", id + "FormPGroup");
+		addAtomicValueWithNameInDataAndValueIfNotExisting("newPresentationFormId",
+				id + "FormNewPGroup");
+		addAtomicValueWithNameInDataAndValueIfNotExisting("menuPresentationViewId",
+				id + "MenuPGroup");
+		addAtomicValueWithNameInDataAndValueIfNotExisting("listPresentationViewId",
+				id + "ListPGroup");
+		addAtomicValueWithNameInDataAndValueIfNotExisting("selfPresentationViewId",
+				id + "ViewSelfPGroup");
+		// addAtomicValueWithNameInDataAndValueIfNotExisting("searchPresentationFormId",
+		// id+"FormSearchPGroup");
 	}
 
 	private void addTexts() {
-		addAtomicValueWithNameInDataAndValueIfNotExisting("textId", id+"Text");
-		addAtomicValueWithNameInDataAndValueIfNotExisting("defTextId", id+"DefText");
+		addAtomicValueWithNameInDataAndValueIfNotExisting("textId", id + "Text");
+		addAtomicValueWithNameInDataAndValueIfNotExisting("defTextId", id + "DefText");
 	}
-
 
 }
--- src/test/java/se/uu/ub/cora/metacreator/recordtype/RecordTypeMetaCompleterTest.java
+++ src/test/java/se/uu/ub/cora/metacreator/recordtype/RecordTypeMetaCompleterTest.java
@@ -1,72 +1,86 @@
 package se.uu.ub.cora.metacreator.recordtype;
 
+import static org.testng.Assert.assertEquals;
+
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import se.uu.ub.cora.metacreator.dependency.SpiderInstanceFactorySpy;
 import se.uu.ub.cora.metacreator.testdata.DataCreator;
 import se.uu.ub.cora.spider.data.SpiderDataGroup;
 import se.uu.ub.cora.spider.dependency.SpiderInstanceProvider;
 
-import static org.testng.Assert.assertEquals;
-
 public class RecordTypeMetaCompleterTest {
-    private SpiderInstanceFactorySpy instanceFactory;
-    private String userId;
-    private RecordTypeMetaCompleter metaCompleter;
+	private SpiderInstanceFactorySpy instanceFactory;
+	private String userId;
+	private RecordTypeMetaCompleter metaCompleter;
 
-    @BeforeMethod
-    public void setUp() {
-        instanceFactory = new SpiderInstanceFactorySpy();
-        SpiderInstanceProvider.setSpiderInstanceFactory(instanceFactory);
-        metaCompleter = new RecordTypeMetaCompleter();
-        userId = "testUser";
-    }
+	@BeforeMethod
+	public void setUp() {
+		instanceFactory = new SpiderInstanceFactorySpy();
+		SpiderInstanceProvider.setSpiderInstanceFactory(instanceFactory);
+		metaCompleter = new RecordTypeMetaCompleter();
+		userId = "testUser";
+	}
 
-    @Test
-    public void testDefaultValuesWhenAllValuesMissing(){
+	@Test
+	public void testDefaultValuesWhenAllValuesMissing() {
 
-        SpiderDataGroup recordType = DataCreator.createSpiderDataGroupForRecordTypeWithId("myRecordType");
-        metaCompleter.useExtendedFunctionality(userId, recordType);
-        assertAllValuesWereAddedCorrectly(recordType);
-    }
+		SpiderDataGroup recordType = DataCreator
+				.createSpiderDataGroupForRecordTypeWithId("myRecordType");
+		metaCompleter.useExtendedFunctionality(userId, recordType);
+		assertAllValuesWereAddedCorrectly(recordType);
+	}
 
-    private void assertAllValuesWereAddedCorrectly(SpiderDataGroup recordType) {
-        assertEquals(recordType.extractAtomicValue("metadataId"), "myRecordTypeGroup");
-        assertEquals(recordType.extractAtomicValue("newMetadataId"), "myRecordTypeNewGroup");
-        assertEquals(recordType.extractAtomicValue("presentationViewId"), "myRecordTypeViewPGroup");
-        assertEquals(recordType.extractAtomicValue("presentationFormId"), "myRecordTypeFormPGroup");
-        assertEquals(recordType.extractAtomicValue("newPresentationFormId"), "myRecordTypeFormNewPGroup");
-        assertEquals(recordType.extractAtomicValue("menuPresentationViewId"), "myRecordTypeMenuPGroup");
-        assertEquals(recordType.extractAtomicValue("listPresentationViewId"), "myRecordTypeListPGroup");
-        assertEquals(recordType.extractAtomicValue("searchMetadataId"), "myRecordTypeSearchGroup");
-        assertEquals(recordType.extractAtomicValue("searchPresentationFormId"), "myRecordTypeFormSearchPGroup");
-        assertEquals(recordType.extractAtomicValue("selfPresentationViewId"), "myRecordTypeViewSelfPGroup");
-        assertEquals(recordType.extractAtomicValue("textId"), "myRecordTypeText");
-        assertEquals(recordType.extractAtomicValue("defTextId"), "myRecordTypeDefText");
-    }
+	private void assertAllValuesWereAddedCorrectly(SpiderDataGroup recordType) {
+		assertEquals(recordType.extractAtomicValue("metadataId"), "myRecordTypeGroup");
+		assertEquals(recordType.extractAtomicValue("newMetadataId"), "myRecordTypeNewGroup");
+		assertEquals(recordType.extractAtomicValue("presentationViewId"), "myRecordTypeViewPGroup");
+		assertEquals(recordType.extractAtomicValue("presentationFormId"), "myRecordTypeFormPGroup");
+		assertEquals(recordType.extractAtomicValue("newPresentationFormId"),
+				"myRecordTypeFormNewPGroup");
+		assertEquals(recordType.extractAtomicValue("menuPresentationViewId"),
+				"myRecordTypeMenuPGroup");
+		assertEquals(recordType.extractAtomicValue("listPresentationViewId"),
+				"myRecordTypeListPGroup");
+		assertEquals(recordType.extractAtomicValue("selfPresentationViewId"),
+				"myRecordTypeViewSelfPGroup");
+		assertEquals(recordType.extractAtomicValue("textId"), "myRecordTypeText");
+		assertEquals(recordType.extractAtomicValue("defTextId"), "myRecordTypeDefText");
 
-    @Test
-    public void testDefaultValuesWhenAllValuesPresent(){
-        SpiderDataGroup recordType = DataCreator.createSpiderDataGroupForRecordTypeWithId("mySpecial");
-        DataCreator.addAllValuesToSpiderDataGroup(recordType, "mySpecial");
+		SpiderDataGroup search = (SpiderDataGroup) recordType.getFirstChildWithNameInData("search");
+		assertEquals(search.extractAtomicValue("linkedRecordId"), "myRecordTypeSearch");
+		// assertEquals(recordType.extractAtomicValue("searchPresentationFormId"),
+		// "myRecordTypeFormSearchPGroup");
+	}
 
-        metaCompleter.useExtendedFunctionality(userId, recordType);
+	@Test
+	public void testDefaultValuesWhenAllValuesPresent() {
+		SpiderDataGroup recordType = DataCreator
+				.createSpiderDataGroupForRecordTypeWithId("mySpecial");
+		DataCreator.addAllValuesToSpiderDataGroup(recordType, "mySpecial");
 
-        assertEquals(recordType.getChildren().size(), 15);
+		metaCompleter.useExtendedFunctionality(userId, recordType);
 
-        assertEquals(recordType.extractAtomicValue("metadataId"), "mySpecialGroup");
-        assertEquals(recordType.extractAtomicValue("newMetadataId"), "mySpecialNewGroup");
-        assertEquals(recordType.extractAtomicValue("presentationViewId"), "mySpecialViewPGroup");
-        assertEquals(recordType.extractAtomicValue("presentationFormId"), "mySpecialFormPGroup");
-        assertEquals(recordType.extractAtomicValue("newPresentationFormId"), "mySpecialFormNewPGroup");
-        assertEquals(recordType.extractAtomicValue("menuPresentationViewId"), "mySpecialMenuPGroup");
-        assertEquals(recordType.extractAtomicValue("listPresentationViewId"), "mySpecialListPGroup");
-        assertEquals(recordType.extractAtomicValue("searchMetadataId"), "mySpecialSearchGroup");
-        assertEquals(recordType.extractAtomicValue("searchPresentationFormId"), "mySpecialFormSearchPGroup");
-        assertEquals(recordType.extractAtomicValue("selfPresentationViewId"), "mySpecialViewSelfPGroup");
-        assertEquals(recordType.extractAtomicValue("textId"), "mySpecialText");
-        assertEquals(recordType.extractAtomicValue("defTextId"), "mySpecialDefText");
-    }
+		assertEquals(recordType.getChildren().size(), 15);
 
+		assertEquals(recordType.extractAtomicValue("metadataId"), "mySpecialGroup");
+		assertEquals(recordType.extractAtomicValue("newMetadataId"), "mySpecialNewGroup");
+		assertEquals(recordType.extractAtomicValue("presentationViewId"), "mySpecialViewPGroup");
+		assertEquals(recordType.extractAtomicValue("presentationFormId"), "mySpecialFormPGroup");
+		assertEquals(recordType.extractAtomicValue("newPresentationFormId"),
+				"mySpecialFormNewPGroup");
+		assertEquals(recordType.extractAtomicValue("menuPresentationViewId"),
+				"mySpecialMenuPGroup");
+		assertEquals(recordType.extractAtomicValue("listPresentationViewId"),
+				"mySpecialListPGroup");
+		assertEquals(recordType.extractAtomicValue("searchMetadataId"), "mySpecialSearchGroup");
+		assertEquals(recordType.extractAtomicValue("searchPresentationFormId"),
+				"mySpecialFormSearchPGroup");
+		assertEquals(recordType.extractAtomicValue("selfPresentationViewId"),
+				"mySpecialViewSelfPGroup");
+		assertEquals(recordType.extractAtomicValue("textId"), "mySpecialText");
+		assertEquals(recordType.extractAtomicValue("defTextId"), "mySpecialDefText");
+	}
 
 }
