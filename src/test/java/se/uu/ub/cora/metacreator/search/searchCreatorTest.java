b53f618cc57aafc82bcb52553272c1643e88858c
maddekenn <madeleine.kennback@ub.uu.se>
2019-12-11 15:01:40 +0000

    Adding factories

--- src/test/java/se/uu/ub/cora/metacreator/search/searchCreatorTest.java
+++ src/test/java/se/uu/ub/cora/metacreator/search/searchCreatorTest.java
@@ -6,48 +6,60 @@ import static org.testng.Assert.assertNotNull;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import se.uu.ub.cora.data.DataAtomicFactory;
+import se.uu.ub.cora.data.DataAtomicProvider;
 import se.uu.ub.cora.data.DataGroup;
+import se.uu.ub.cora.data.DataGroupFactory;
+import se.uu.ub.cora.data.DataGroupProvider;
 import se.uu.ub.cora.metacreator.dependency.SpiderInstanceFactorySpy;
+import se.uu.ub.cora.metacreator.recordtype.DataAtomicFactorySpy;
+import se.uu.ub.cora.metacreator.recordtype.DataGroupFactorySpy;
 import se.uu.ub.cora.metacreator.testdata.DataCreator;
 import se.uu.ub.cora.spider.dependency.SpiderInstanceProvider;
 
 public class searchCreatorTest {
-    private SpiderInstanceFactorySpy instanceFactory;
-    private String authToken;
+	private SpiderInstanceFactorySpy instanceFactory;
+	private String authToken;
+	private DataGroupFactory dataGroupFactory;
+	private DataAtomicFactory dataAtomicFactory;
 
-    @BeforeMethod
-    public void setUp() {
-        instanceFactory = new SpiderInstanceFactorySpy();
-        SpiderInstanceProvider.setSpiderInstanceFactory(instanceFactory);
-        authToken = "testUser";
-    }
+	@BeforeMethod
+	public void setUp() {
+		dataGroupFactory = new DataGroupFactorySpy();
+		DataGroupProvider.setDataGroupFactory(dataGroupFactory);
+		instanceFactory = new SpiderInstanceFactorySpy();
+		dataAtomicFactory = new DataAtomicFactorySpy();
+		DataAtomicProvider.setDataAtomicFactory(dataAtomicFactory);
+		SpiderInstanceProvider.setSpiderInstanceFactory(instanceFactory);
+		authToken = "testUser";
+	}
 
-    @Test
-    public void testCreateTextNoTextExists(){
-        DataGroup search = DataCreator.createSearchWithId("someSearch");
-        DataCreator.addRecordLinkWithNameInDataAndLinkedRecordTypeAndLinkedRecordId(
-                search, "textId", "textSystemOne", "someNonExistingText");
-        DataCreator.addRecordLinkWithNameInDataAndLinkedRecordTypeAndLinkedRecordId(
-                search, "defTextId", "textSystemOne", "someNonExistingDefText");
+	@Test
+	public void testCreateTextNoTextExists() {
+		DataGroup search = DataCreator.createSearchWithId("someSearch");
+		DataCreator.addRecordLinkWithNameInDataAndLinkedRecordTypeAndLinkedRecordId(search,
+				"textId", "textSystemOne", "someNonExistingText");
+		DataCreator.addRecordLinkWithNameInDataAndLinkedRecordTypeAndLinkedRecordId(search,
+				"defTextId", "textSystemOne", "someNonExistingDefText");
 
-        SearchCreator creator = SearchCreator.forImplementingTextType("textSystemOne");
-        assertNotNull(creator);
-        creator.useExtendedFunctionality(authToken, search);
+		SearchCreator creator = SearchCreator.forImplementingTextType("textSystemOne");
+		assertNotNull(creator);
+		creator.useExtendedFunctionality(authToken, search);
 
-        assertEquals(instanceFactory.spiderRecordCreators.size(), 2);
-    }
+		assertEquals(instanceFactory.spiderRecordCreators.size(), 2);
+	}
 
-    @Test
-    public void testCreateTextWhenTextExists(){
-        DataGroup search = DataCreator.createSearchWithId("someSearch");
-        DataCreator.addRecordLinkWithNameInDataAndLinkedRecordTypeAndLinkedRecordId(
-                search, "textId", "textSystemOne", "someExistingText");
-        DataCreator.addRecordLinkWithNameInDataAndLinkedRecordTypeAndLinkedRecordId(
-                search, "defTextId", "textSystemOne", "someExistingDefText");
+	@Test
+	public void testCreateTextWhenTextExists() {
+		DataGroup search = DataCreator.createSearchWithId("someSearch");
+		DataCreator.addRecordLinkWithNameInDataAndLinkedRecordTypeAndLinkedRecordId(search,
+				"textId", "textSystemOne", "someExistingText");
+		DataCreator.addRecordLinkWithNameInDataAndLinkedRecordTypeAndLinkedRecordId(search,
+				"defTextId", "textSystemOne", "someExistingDefText");
 
-        SearchCreator creator = SearchCreator.forImplementingTextType("textSystemOne");
-        creator.useExtendedFunctionality(authToken, search);
+		SearchCreator creator = SearchCreator.forImplementingTextType("textSystemOne");
+		creator.useExtendedFunctionality(authToken, search);
 
-        assertEquals(instanceFactory.spiderRecordCreators.size(), 0);
-    }
+		assertEquals(instanceFactory.spiderRecordCreators.size(), 0);
+	}
 }
